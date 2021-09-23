0bd7c482e0bb4da889e607e27a560a4f6c73308b
tranquocthien <tranquocthien@aol.com>
2021-09-23 23:44:24 +0700

    trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-     <h1> hello</h1>
+    <div className="trello-wrapper">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">board bar</nav>
+      <div className="board-columns">
+        <header> brainstore</header>
+        <ul>
+          <li>
+            Add what you'd like to work on bellow
+          </li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
     </div>
   );
 }
